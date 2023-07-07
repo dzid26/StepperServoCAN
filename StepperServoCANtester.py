b9eba8e38e73286245b6078798e5be01c000ace1
killinen <bhepatiitti@gmail.com>
2023-07-07 23:38:42 +0300

    StepperServoCANtester bug fixes and improvements - Delete Connect button - Connect to chosen CAN interface when picked from dropdown list - Enable floating point numbers for torque values - Start sending CAN msg if default CAN interface is found on startup - If selected CAN interface is not found, inform the matter - If valid interface is selected and after that non valid interface is choosen, stop CAN msg sending - Fix unsuccessuful program terminationbug when closing the window from x button - Fix bug regarding to handling non existing socketCAN channels - Fix wrong MAX_TORQUE value

--- StepperServoCANtester.py
+++ StepperServoCANtester.py
@@ -21,7 +21,7 @@ import can
 
 # Constants
 MIN_TORQUE = -16
-MAX_TORQUE = 16
+MAX_TORQUE = 15.875
 MIN_ANGLE = -4096
 MAX_ANGLE = 4096
 
@@ -59,51 +59,59 @@ def msg_calc_checksum_8bit(data: bytes, len: int, msg_id: int) -> int:
 
 # This function configures the socketCAN interface in the terminal
 def configure_socketcan():
-
     # Check for available socketCAN channels
-    channels = []
-    output = subprocess.check_output("ip -details -brief link | grep can", shell=True)
-    for line in output.decode().split("\n"):
-        if line.strip():
-            words = line.split()
-            if words[1] == "UP" or words[1] == "UNKNOWN":  # Check if channel is up
-                channel = words[0]
-                channels.append(channel)
-    # If multiple CAN channels are found, list them and ask in terminal which channel to choose
+    try:
+        channels = []
+        output = subprocess.check_output("ip -details -brief link | grep can", shell=True)
+        for line in output.decode().split("\n"):
+            if line.strip():
+                words = line.split()
+                if words[1] == "UP" or words[1] == "UNKNOWN":  # Check if channel is up
+                    channel = words[0]
+                    channels.append(channel)
+    except subprocess.CalledProcessError:
+        print("No socketCAN channels found.")
+        print("Hint: if you want to test the program set up a virtual CAN channel.")
+        print("sudo modprobe vcan")
+        print("sudo ip link add dev vcan0 type vcan")
+        print("sudo ip link set up vcan0")
+        return None
+
+    # If multiple CAN channels are found, list them and ask in the terminal which channel to choose
     if len(channels) > 1:
         print("Multiple CAN channels found:")
         for i, channel in enumerate(channels):
             print(f"{i+1}: {channel}")
         print(f"{len(channels)+1}: Abort the program")
-        selection = input("Select an channel number: ")
+        selection = input("Select a channel number: ")
         try:
             selection = int(selection)
-            if selection < 1 or selection > len(channels)+1:
+            if selection < 1 or selection > len(channels) + 1:
                 raise ValueError
-            elif selection == len(channels)+1:
+            elif selection == len(channels) + 1:
                 print("Aborting program...")
-                return
+                return None
         except ValueError:
             print("Invalid selection")
-            return
-        channel = channels[selection-1]
-    # If only 1 channel found, just choose that
+            return None
+        channel = channels[selection - 1]
+    # If only 1 channel is found, choose that
     elif len(channels) == 1:
         channel = channels[0]
-    # If no channels found abort the program
+    # If no channels are found, abort the program
     else:
-        print("No CAN channels found that are UP, will abort the program.")
-        print("Hint: if you want to test the program set up virtual CAN channel with these commands.")
+        print("No socketCAN channels found that are UP.")
+        print("Hint: if you want to test the program set up a virtual CAN channel.")
         print("sudo modprobe vcan")
         print("sudo ip link add dev vcan0 type vcan")
         print("sudo ip link set up vcan0")
+        return None
     return channel
 
+# Connect to CAN interface that has been selected from GUI and start sending CAN msgs
 def connect_to_can_interface(interface):
-    """
-    This function searches for available CAN interfaces, prompts the user to select one if there are multiple available.
-    It then connects to the selected CAN interface using the python-can library and returns a can_bus object.
-    """
+    global can_enabled
+    can_enabled = True
     interface_name = interface.get()
     channel = None
     if interface_name == "socketcan":
@@ -111,32 +119,39 @@ def connect_to_can_interface(interface):
             return
         channel = configure_socketcan()
 
-    # # Connect to selected CAN interface
-    # print(f"Connected to CAN interface {interface}")
+    if channel is None:
+        print(f'CAN interface "{interface_name}" not available.')
+        # If send_message thread is already running, stop the while loop by setting can_enable falg to False
+        can_enabled = False
+        # Handle the case where there are no compatible CAN interfaces
+        # You can display an error message or perform other actions as needed
+        return
+
     can_bus = can.Bus(interface=interface_name, bitrate=500000, channel=channel)
 
-    # Start the send_can_message function in a separate thread
-    thread = threading.Thread(target=lambda:send_message(can_bus))
+    thread = threading.Thread(target=lambda: send_message(can_bus))
     thread.start()
 
     return can_bus
 
+def interface_selected(event):
+    # Run the connect_to_can_interface function when a new option is selected
+    interface = selected_backend
+    connect_to_can_interface(interface)
+
 def validate_input(input_str, min_value, max_value):
     try:
-        # Try to convert the input string to an integer
-        value = int(input_str)
-
-        # Check if the integer value is within the specified range
+        # Convert the input string to an floating point number
+        value = float(input_str)
+        # Check if the value is within the specified range
         if min_value <= value <= max_value:
-            return value  # If so, return the integer value
+            return value
     except ValueError:
-        # If there was an error converting to an integer, or the value is outside the range,
+        # If there was an error converting to an float, or the value is outside the range,
         # catch the exception and do nothing
         pass
-
     # If we get here, the input was invalid, so return None
     return None
-
 
 # Function to update torque and angle from widget values
 def update_values():
@@ -152,7 +167,7 @@ def update_values():
     # If the input is invalid, set the torque to zero and show an error message
     if torque is None:
         torque = 0
-        messagebox.showerror("Error", "Torque value should be between -16 and 16")
+        messagebox.showerror("Error", "Torque value should be between {} and {}".format(MIN_TORQUE, MAX_TORQUE))
 
     # Get the angle input string from the widget
     angle_str = angle_widget.get()
@@ -165,12 +180,9 @@ def update_values():
         angle = 0
         messagebox.showerror("Error", "Angle value should be between -180 and 180")
 
-
 # Function to encode and send the CAN message
 def update_message():
     global msg, data, counter, torque, angle
-
-    # msg = db.get_message_by_name('STEERING_COMMAND')
 
     # Calculate new counter value
     counter = counter + 1
@@ -229,7 +241,6 @@ def send_message(can_bus):
             loop_count = 0
             last_print_time = time.monotonic()
 
-
 # Define a class named SteerModeWidget
 class SteerModeWidget:
     def __init__(self, master, label_text, options):
@@ -262,7 +273,6 @@ class SteerModeWidget:
 ##########################################################################
 ############################ DEFINE VARIABLES ############################
 ##########################################################################
-
 
 # Flag to control the CAN traffic
 can_enabled = True
@@ -301,26 +311,26 @@ window = tk.Tk()
 window.title("StepperServoCAN Tester")
 
 # Set window width and height to custom values
-window.geometry("360x260")  # Set window width and height
+window.geometry("360x250")  # Set window width and height
 
 # Create labels for the widgets
 selected_backend = tk.StringVar(window)
-selected_backend.set('pcan') #default
+selected_backend.set('pcan')
 
-can_interface_selector = ttk.OptionMenu(window, selected_backend, selected_backend.get(), *sorted(can.interfaces.VALID_INTERFACES))
-can_interface_connect = tk.Button(window, text="Connect", command=lambda:connect_to_can_interface(selected_backend))
+can_interface_selector = ttk.Combobox(window, values=[*sorted(can.interfaces.VALID_INTERFACES)], textvariable=selected_backend)
+can_interface_selector.bind("<<ComboboxSelected>>", interface_selected)
 
 can_label = tk.Label(window, text="CAN interface:       ")
 torque_label = tk.Label(window, text="Steer Torque:       ")
 angle_label = tk.Label(window, text="Steer Angle:       ")
-
 
 # Set the initial values for torque and angle
 initial_torque = torque
 initial_angle = angle
 
 # Create the torque and angle widget with an initial value
-torque_widget = tk.Entry(window, textvariable=tk.StringVar(value=str(initial_torque)))
+torque_var = tk.DoubleVar(value=initial_torque)
+torque_widget = tk.Entry(window, textvariable=torque_var)
 angle_widget = tk.Entry(window, textvariable=tk.StringVar(value=str(initial_angle)))
 
 # Add SteerModeWidget for the Steer Mode option
@@ -339,12 +349,11 @@ send_button = tk.Button(window, text='Update Torque/Angle value', command=update
 # Place the labels and widgets using grid
 can_label.grid(row=0, column=0, sticky="w")  # set sticky to "w" for left alignment
 can_interface_selector.grid(row=0, column=1, sticky="w")
-can_interface_connect.grid(row=0, column=2, sticky="w")
 torque_label.grid(row=1, column=0, sticky="w")  # set sticky to "w" for left alignment
 torque_widget.grid(row=1, column=1, sticky="w")
 angle_label.grid(row=2, column=0, sticky="w")  # set sticky to "w" for left alignment
 angle_widget.grid(row=2, column=1, sticky="w")
-send_button.grid(row=8, column=0, columnspan=2)
+send_button.grid(row=9, column=0, columnspan=2, pady=(10, 0))
 
 # Function for closing the program elegantly
 def on_closing():
@@ -354,10 +363,17 @@ def on_closing():
 
 # Add the quit button
 quit_button = tk.Button(window, text="Quit", command=on_closing)
-quit_button.grid(row=10, column=0, columnspan=2, pady=10, sticky=tk.N+tk.S+tk.E+tk.W)
+quit_button.grid(row=10, column=0, columnspan=2, pady=(10, 0), sticky=tk.N+tk.S+tk.E+tk.W)
 
+# Trigger events when Esc and Return key are pressed
 window.bind('<Escape>', lambda event: on_closing())
 window.bind('<Return>', lambda event: update_values())
+
+# This is for making successful program termination when GUI closed from top right corner x button
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+# On startup try to connect to default CAN interface
+connect_to_can_interface(selected_backend)
 
 # Run the GUI loop
 window.mainloop()
