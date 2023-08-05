52f2da75bec5a453ce1447a5b965d4f7f0193ec0
dzid26 <dzidmail@gmail.com>
2023-08-05 23:32:19 +0100

    Added unit testing on the target

--- firmware/linker_options_pio.py
+++ firmware/linker_options_pio.py
@@ -15,3 +15,21 @@ if(env.GetBuildType()=="release"):
             "--specs=nano.specs",
         ],
     )
+
+
+if(env.GetBuildType()=="debug+test"):
+    env.Append(
+    # Remove stub implementations
+    BUILD_UNFLAGS=[
+        "-lnosys",
+        "--specs=nosys.specs",
+    ],
+
+    # Use the semihosted version of the syscalls
+    LINKFLAGS=[
+        "--specs=rdimon.specs",
+    ],
+    LIBS=[
+        "rdimon",
+    ],
+)
