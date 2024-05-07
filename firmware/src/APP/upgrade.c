8aec123c0fb653830326d138cf8ff500a69e3ce7
dzid26 <dzidmail@gmail.com>
2024-05-07 15:43:05 +0100

    Prepare for v3 - flash this commit to downgrade

--- firmware/src/APP/upgrade.c
+++ firmware/src/APP/upgrade.c
@@ -23,6 +23,18 @@ static void save_current_fw_version(void){
 
 
 void app_upgrade_begin(void){
+
+	//downgrade v3 -> v2
+	if((read_previous_fw_version() >= 3000U) && (read_current_fw_version() == 2999U)){
+		// 0.2 -> 0.3
+		// upgrade angle cals to new voltage-control that uses different uses canonical Parke transformation
+		FlashCalData_t updated_cal;
+		updated_cal.status = nvmFlashCalData->status; 	// cppcheck-suppress  misra-c2012-11.4 - loading values from mapped flash structure
+		for (uint16_t i=0; i < CALIBRATION_TABLE_SIZE; ++i ){
+			updated_cal.FlashCalData[i] = nvmFlashCalData->FlashCalData[i] - (uint16_t)(ANGLE_STEPS / nvmMirror.motorParams.fullStepsPerRotation); // cppcheck-suppress  misra-c2012-11.4 - loading values from mapped flash structure
+		}
+		nvmWriteCalTable(&updated_cal);
+	}
 	
 	save_current_fw_version();
 }
