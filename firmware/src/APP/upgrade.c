95df7d4a837cdfbbb94b2f806040bd789685666b
dzid26 <dzidmail@gmail.com>
2024-05-18 02:01:26 +0100

    refactor: upgrade version and linter suppressions

--- firmware/src/APP/upgrade.c
+++ firmware/src/APP/upgrade.c
@@ -23,25 +23,28 @@ static void save_current_fw_version(void){
 
 
 void app_upgrade_begin(void){
+	uint16_t version_upgrade;
 	//upgrade v2 -> v3
-	if((read_previous_fw_version() < 3000U) && (read_current_fw_version() >= 3000U)){
+	version_upgrade = 3000U;
+	if((read_previous_fw_version() < version_upgrade) && (read_current_fw_version() >= version_upgrade)){  // cppcheck-suppress  knownConditionTrueFalse
 		// upgrade angle cals to new voltage-control that uses different uses canonical Parke transformation
 		FlashCalData_t updated_cal;
-		updated_cal.status = nvmFlashCalData->status; 	// cppcheck-suppress  misra-c2012-11.4 - loading values from mapped flash structure
+		updated_cal.status = nvmFlashCalData->status;
 		for (uint16_t i=0; i < CALIBRATION_TABLE_SIZE; ++i ){
-			updated_cal.FlashCalData[i] = nvmFlashCalData->FlashCalData[i] + (uint16_t)(ANGLE_STEPS / nvmMirror.motorParams.fullStepsPerRotation); // cppcheck-suppress  misra-c2012-11.4 - loading values from mapped flash structure
+			updated_cal.FlashCalData[i] = nvmFlashCalData->FlashCalData[i] + (uint16_t)(ANGLE_STEPS / nvmMirror.motorParams.fullStepsPerRotation);
 		}
 		nvmWriteCalTable(&updated_cal);
 	}
 
 	//downgrade v3 -> v2
-	if((read_previous_fw_version() >= 3000U) && (read_current_fw_version() == 2999U)){
+	version_upgrade = 3000U;
+	if((read_previous_fw_version() >= version_upgrade) && (read_current_fw_version() == (version_upgrade-1U))){  // cppcheck-suppress  knownConditionTrueFalse
 		// 0.2 -> 0.3
 		// upgrade angle cals to new voltage-control that uses different uses canonical Parke transformation
 		FlashCalData_t updated_cal;
-		updated_cal.status = nvmFlashCalData->status; 	// cppcheck-suppress  misra-c2012-11.4 - loading values from mapped flash structure
+		updated_cal.status = nvmFlashCalData->status;
 		for (uint16_t i=0; i < CALIBRATION_TABLE_SIZE; ++i ){
-			updated_cal.FlashCalData[i] = nvmFlashCalData->FlashCalData[i] - (uint16_t)(ANGLE_STEPS / nvmMirror.motorParams.fullStepsPerRotation); // cppcheck-suppress  misra-c2012-11.4 - loading values from mapped flash structure
+			updated_cal.FlashCalData[i] = nvmFlashCalData->FlashCalData[i] - (uint16_t)(ANGLE_STEPS / nvmMirror.motorParams.fullStepsPerRotation);
 		}
 		nvmWriteCalTable(&updated_cal);
 	}
