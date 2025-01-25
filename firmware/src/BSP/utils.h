69db08644a428c69c8faf651d21fb8322766092f
dzid26 <dzidmail@gmail.com>
2025-01-25 18:10:20 +0000

    allow braced-group within expression gcc extension

--- firmware/src/BSP/utils.h
+++ firmware/src/BSP/utils.h
@@ -3,22 +3,26 @@
 
 #include <stdint.h>
 
+// cppcheck-suppress-macro misra-c2012-1.2
 #define min(a,b)             \
-({                           \
+__extension__ ({             \
     __typeof__ (a) _a = (a); \
     __typeof__ (b) _b = (b); \
-    (_a < _b) ? _a : _b;       \
+    (_a < _b) ? _a : _b;     \
 })
 
+// cppcheck-suppress-macro misra-c2012-1.2
 #define max(a,b)             \
-({                           \
+__extension__ ({             \
     __typeof__ (a) _a = (a); \
     __typeof__ (b) _b = (b); \
     (_a > _b) ? _a : _b;     \
 })
 
 
-#define clip(x, low, high) ({\
+// cppcheck-suppress-macro misra-c2012-1.2
+#define clip(x, low, high) \
+__extension__ ({\
   __typeof__(x) _x = (x); \
   __typeof__(low) _low = (low);\
   __typeof__(high) _high = (high);\
