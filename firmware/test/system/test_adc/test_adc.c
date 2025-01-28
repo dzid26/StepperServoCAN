316ebb0b271667ed4597f367b156b47d0ab3dbfc
dzid26 <dzidmail@gmail.com>
2025-01-28 11:48:06 +0000

    test values

--- firmware/test/system/test_adc/test_adc.c
+++ firmware/test/system/test_adc/test_adc.c
@@ -29,14 +29,14 @@ void tearDown(void) {
 #include "motor.h"
 #include "delay.h"
 
-static void test_adc_values(void) {
-    
+static void test_normal_adc_values(void) {
+    TEST_ASSERT_FLOAT_WITHIN(.05, 3.366, GetVDDA());
     TEST_ASSERT_FLOAT_WITHIN(.15, 12, GetSupplyVoltage());
-    TEST_ASSERT_FLOAT_WITHIN(.2, 11.8, GetMotorVoltage());
+    TEST_ASSERT_FLOAT_WITHIN(.2, GetSupplyVoltage()-0.1, GetMotorVoltage());
     TEST_ASSERT(GetSupplyVoltage() >= GetMotorVoltage()); //there should be voltage drop due to reverse polarity protection Schottky diode
     TEST_ASSERT_FLOAT_WITHIN(.1, 0, Get_PhaseA_Current());
     TEST_ASSERT_FLOAT_WITHIN(.1, 0, Get_PhaseB_Current());
-    TEST_ASSERT_FLOAT_WITHIN(10, 25, GetChipTemp());
+    TEST_ASSERT_FLOAT_WITHIN(10, 20, GetChipTemp());
 }
 
 
@@ -44,28 +44,28 @@ static void test_adc_values(void) {
 static void test_adc_values_under_load(void) {
     // TEST_IGNORE_MESSAGE("No motor ignore")
     A4950_enable(true);
-    openloop_step(FULLSTEP_ELECTRIC_ANGLE*0, 3300);
+    openloop_step(FULLSTEP_ELECTRIC_ANGLE*0, 3500);
     delay_ms(1000);
 
     adc_update_all(); 
     TEST_ASSERT_FLOAT_WITHIN(2, 10, GetSupplyVoltage());
-    TEST_ASSERT_FLOAT_WITHIN(2, 10, GetMotorVoltage());
+    TEST_ASSERT_FLOAT_WITHIN(1, GetSupplyVoltage()-0.1, GetMotorVoltage());
     TEST_ASSERT(GetSupplyVoltage() > GetMotorVoltage()); //there should be voltage drop due to reverse polarity protection Schottky diode
     TEST_ASSERT_FLOAT_WITHIN(1, 3, Get_PhaseA_Current());
     TEST_ASSERT_FLOAT_WITHIN(.1, 0, Get_PhaseB_Current()); //todo proper current ADC accuracy
-    TEST_ASSERT_FLOAT_WITHIN(10, 27, GetChipTemp());
+    TEST_ASSERT_FLOAT_WITHIN(10, 22, GetChipTemp());
 
 
-    openloop_step(FULLSTEP_ELECTRIC_ANGLE*1, 3300);
+    openloop_step(FULLSTEP_ELECTRIC_ANGLE*1, 3500);
     delay_ms(1000);
 
     adc_update_all(); 
     TEST_ASSERT_FLOAT_WITHIN(2, 10, GetSupplyVoltage());
     TEST_ASSERT_FLOAT_WITHIN(2, 10, GetMotorVoltage());
     TEST_ASSERT(GetSupplyVoltage() > GetMotorVoltage()); //there should be voltage drop due to reverse polarity protection Schottky diode
-    TEST_ASSERT_FLOAT_WITHIN(1, 0, Get_PhaseA_Current());
-    TEST_ASSERT_FLOAT_WITHIN(.1, 3, Get_PhaseB_Current()); //todo proper current ADC accuracy
-    TEST_ASSERT_FLOAT_WITHIN(10, 29, GetChipTemp());
+    TEST_ASSERT_FLOAT_WITHIN(.1, 0, Get_PhaseA_Current());
+    TEST_ASSERT_FLOAT_WITHIN(1, 3, Get_PhaseB_Current()); //todo proper current ADC accuracy
+    TEST_ASSERT_FLOAT_WITHIN(10, 24, GetChipTemp());
 
 }
 
@@ -78,9 +78,9 @@ int main(void) {
     printf("ADC clock: %lu Hz\n", clks.ADCCLK_Frequency);
     printf("VDDA: %f V\n", GetVDDA());
 
-    RUN_TEST(test_adc_values);
+    RUN_TEST(test_normal_adc_values);
     RUN_TEST(test_adc_values_under_load);
     delay_ms(3000);      //let the supply stabilize, if not Vmot might be above Vinn, due to inductance
-    RUN_TEST(test_adc_values);
+    RUN_TEST(test_normal_adc_values);
     return UNITY_END();
 }
